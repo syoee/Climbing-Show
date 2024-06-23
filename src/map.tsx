a6612d7ed4b485e634f2638f0d0dd6a9de1f17d1
syoee <tjsdyd1347@gmail.com>
2024-06-23 23:17:18 +0900

    Modify: Map

--- src/map.tsx
+++ src/map.tsx
@@ -1,42 +1,23 @@
-import React, { useEffect, useRef } from "react";
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>지도 생성하기</title>
+    
+</head>
+<body>
+<!-- 지도를 표시할 div 입니다 -->
+<div id="map" style="width:100%;height:350px;"></div>
 
-const MapComponent = ({ latitude, longitude }) => {
-	const container = useRef(null); // 지도를 담을 영역의 DOM 레퍼런스
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=발급받은 APP KEY를 사용하세요"></script>
+<script>
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
 
-	useEffect(() => {
-		const script = document.createElement("script");
-		script.async = true;
-		script.src =
-			"https://dapi.kakao.com/v2/maps/sdk.js?appkey=YOUR_APP_KEY&autoload=false";
-		document.head.appendChild(script);
-
-		script.onload = () => {
-			kakao.maps.load(() => {
-				// v3가 모두 로드된 후, 이 콜백 함수가 실행됩니다.
-				const options = {
-					// 지도를 생성할 때 필요한 기본 옵션
-					center: new kakao.maps.LatLng(latitude, longitude), // 지도의 중심좌표.
-					level: 3, // 지도의 레벨(확대, 축소 정도)
-				};
-
-				const map = new kakao.maps.Map(container.current, options); // 지도 생성 및 객체 리턴
-
-				const markerPosition = new kakao.maps.LatLng(latitude, longitude);
-				const marker = new kakao.maps.Marker({
-					position: markerPosition,
-				});
-				marker.setMap(map);
-			});
-		};
-	}, [latitude, longitude]);
-
-	return (
-		<div
-			id="map"
-			style={{ width: "500px", height: "400px" }}
-			ref={container}
-		></div>
-	);
-};
-
-export default MapComponent;
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption); 
+</script>
+</body>
+</html>
