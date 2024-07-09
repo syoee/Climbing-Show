31d652383f34e70ffd64dcb229b7c4fb10031c91
syoee <tjsdyd1347@gmail.com>
2024-07-09 13:59:49 +0900

    Trouble Shooting

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -6,33 +6,34 @@ const SearchBar = () => {
 	const [query, setQuery] = useState<string>(''); // 검색어 상태를 관리
 	const [data, setData] = useState<any>();
 
-	const handleSearch = async (event: FormEvent) => {
-		event.preventDefault(); // 페이지 새로 고침을 방지
-
+	const fetchData = async (searchQuery: string) => {
 		try {
 			const res = await axios.get(`http://localhost:8080/climbing-info`, {
 				params: {
 					searchKey: 'name',
-					searchValue: query, // 검색어(query)를 서버로 전송
+					searchValue: searchQuery, // 검색어(query)를 서버로 전송
 				},
 			});
 
 			console.log('서버 응답 데이터:', res.data);
-			// 여기에서 서버 응답 데이터를 처리하거나 다른 작업을 수행 가능
-			setData(res.data);
+			setData(res.data); // 서버 응답 데이터를 상태로 설정
 		} catch (error) {
 			console.error('데이터를 가져오는 중 에러 발생:', error);
 		}
 	};
 
+	const handleSearch = (event: FormEvent) => {
+		event.preventDefault(); // 페이지 새로 고침을 방지
+		fetchData(query); // 입력된 검색어로 데이터 가져오기
+	};
+
 	useEffect(() => {
-		// 초기 렌더링 시에도 데이터를 가져오도록 수정
-		handleSearch(data);
+		fetchData(''); // 초기 빈 검색어로 데이터 가져오기
 	}, []);
 
 	return (
 		<div className="py-5 flex items-center justify-center">
-			<form className="flex items-center" onSubmit={(e) => handleSearch(e)}>
+			<form className="flex items-center" onSubmit={handleSearch}>
 				<div className="px-4 py-2 border rounded-md mr-2 flex items-center">
 					<SearchIcon className="text-[#9BA3AF]" />
 					<input
