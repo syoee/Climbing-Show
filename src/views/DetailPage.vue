3aefe36df533ea0ae778a9071a9e233488df4cee
syoee <tjsdyd1347@gmail.com>
2024-08-27 23:35:02 +0900

    Modify: ui

--- src/views/DetailPage.vue
+++ src/views/DetailPage.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="mx-5">
-		<div v-if="results.length > 0" class="flex flex-row justify-around">
-			<div class="w-1/4 mr-3">
+		<div v-if="center" class="flex flex-row justify-around">
+			<div class="w-1/6 mr-3">
 				<div class="w-full aspect-square bg-stone-300 rounded-lg">
 					<img
 						:src="center.logo_url"
@@ -9,40 +9,14 @@
 						class="w-full h-full object-cover rounded-lg"
 					/>
 				</div>
-				<ul class="mt-5">
-					<li
-						v-for="center in results"
-						:key="center.name"
-						class="bg-emerald-300"
-					>
-						<div
-							class="w-full aspect-square border-2 border-neutral-400 rounded-lg"
-						>
-							<img
-								:src="center.logo_url"
-								alt="Nearby Center Logo"
-								class="w-full h-full object-cover rounded-lg"
-							/>
-						</div>
-						<div class="border-2 border-neutral-400 text-xl font-semibold">
-							{{ center.name }}
-						</div>
-						<div class="mt-1 border-2 border-neutral-400 text-lg font-normal">
-							{{ center.address }}
-						</div>
-					</li>
-				</ul>
 			</div>
-			<div class="w-[81%]">
-				<KakaoMap
-					:locations="searchLocations"
-					class="mr-5 mb-3 h-1/2 bg-violet-300 rounded-lg"
-				/>
-				<div class="bg-blue-500 text-2xl font-bold">
+			<div class="w-5/6">
+				<KakaoMap :locations="searchLocations" class="w-[30%]" />
+				<div class="text-2xl font-bold">
 					{{ center.name }}
 				</div>
-				<div class="bg-blue-500 text-lg font-medium">
-					{{ center.address }}
+				<div class="text-lg font-medium">
+					{{ center.address_road }}
 				</div>
 			</div>
 		</div>
@@ -61,18 +35,18 @@ export default {
 	data() {
 		return {
 			id: null,
-			results: [],
+			center: null,
 		};
 	},
 
 	computed: {
 		// KakaoMap에 전달할 위치 데이터를 계산
 		searchLocations() {
-			return this.results
+			return this.center
 				? [
 						{
 							name: this.center.name,
-							address: this.center.address,
+							address: this.center.address_road,
 							position: [this.center.latitude, this.center.longitude],
 						},
 				  ]
@@ -91,8 +65,8 @@ export default {
 				const res = await axios.get(
 					`${process.env.VUE_APP_API_HOST}/climbing-info/${this.id}`
 				);
-				this.results = res.data;
-				console.log(res.data);
+				// API로부터 받은 데이터를 center 저장
+				this.center = res.data;
 			} catch (err) {
 				console.log('error', err);
 			}
