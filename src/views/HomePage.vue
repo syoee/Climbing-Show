6245349fd6c4b9c375cf67af5f7b62e18dbcd88c
syoee <tjsdyd1347@gmail.com>
2024-09-12 20:11:04 +0900

    test

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -188,7 +188,7 @@ export default {
 		reSearchNearbyClimbingCenters() {
 			this.isLoading = true; // 재검색 시 로딩 상태로 변경
 			this.searchNearbyClimbingCenters(this.mapCenterLocation).then(() => {
-				this.showReSearchButton = false; // 재검색 후 버튼 숨기기
+				this.showReSearchButton = false; // 재검색 후 버튼숨기기
 			});
 		},
 	},
