1ed8274f30133493f77d0841ed69d28041cb9ed2
syoee <tjsdyd1347@gmail.com>
2024-11-17 17:22:54 +0900

    Modify: capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,12 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig & {
 	allowNavigation?: string[];
 	androidScheme?: string;
+	iosScheme?: string;
 	hostname?: string;
 } = {
 	appId: 'com.climbingshow.app',
 	appName: 'Climbing-Show',
 	hostname: 'www.climbing-show.com', // 실제 배포된 URL
 	androidScheme: 'http',
+	iosScheme: 'http',
 	webDir: 'dist',
 	bundledWebRuntime: false,
 	allowNavigation: [
