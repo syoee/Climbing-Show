0f59d6cb550dfd781be8997f08fab68cb9e14958
syoee <tjsdyd1347@gmail.com>
2024-11-19 20:07:47 +0900

    Modify: capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -11,12 +11,8 @@ const config: CapacitorConfig & {
 	hostname: 'www.climbing-show.com', // 실제 배포된 URL
 	webDir: 'dist',
 	bundledWebRuntime: false,
-	server: {
-		url: 'http://localhost:3000', // 개발 환경 URL
-		cleartext: true, // HTTP 사용 허용
-		iosScheme: 'http', // ios에서 HTTP 스키마 사용
-		androidScheme: 'http', // android에서 HTTP 스키마 사용
-	},
+	iosScheme: 'http', // ios에서 HTTP 스키마 사용
+	androidScheme: 'http', // android에서 HTTP 스키마 사용
 	allowNavigation: [
 		'www.climbing-show.com',
 		'localhost',
