ce9bda48d771f9d6a7968521a7af2226b327c726
syoee <tjsdyd1347@gmail.com>
2024-12-13 22:43:37 +0900

    ADD: EventInfoPage

--- src/views/EventInfoPage.vue
+++ src/views/EventInfoPage.vue
@@ -1,7 +1,23 @@
 <template>
-	<div>11</div>
+	<div>
+		<div>
+			<div>이번 주 mission</div>
+			<span> 피커스</span><span>를 잡아라!</span>
+		</div>
+		<div>
+			<div>크루 랭킹</div>
+			<button
+				@click="rankInfo"
+				class="w-[1rem] bg-gray-400 text-white text-xs opacity-50 rounded-full aspect-square"
+			>
+				?
+			</button>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {};
+export default {
+	methods: {},
+};
 </script>
