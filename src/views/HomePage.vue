67124a26d7ea5223c2e011f86b9453cacb371e30
syoee <tjsdyd1347@gmail.com>
2024-12-30 16:58:16 +0900

    Modify: HomePage UI

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,7 +4,7 @@
 			src="https://velog.velcdn.com/images/syo_ee/post/393dddb6-15db-4ddd-b1b9-fb0fd178861e/image.jpg"
 			alt="goToHomePageIcon"
 			@click="goHome"
-			class="w-1/4 absolute top-2 left-3"
+			class="w-1/4 absolute top-2 left-3 z-50"
 		/>
 		<img
 			src="https://velog.velcdn.com/images/syo_ee/post/963ce5ab-aec2-4750-b69e-908789de39a2/image.png"
